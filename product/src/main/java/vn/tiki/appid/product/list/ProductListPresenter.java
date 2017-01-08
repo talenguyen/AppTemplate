5b13c5190eccd34e5ea1af60ba64a8c4afc24bd7
Giang Nguyen <giangnguyen.tale@gmail.com>
2017-01-08 15:25:46 +0700

    refactor use IxJava for list handling

--- product/src/main/java/vn/tiki/appid/product/list/ProductListPresenter.java
+++ product/src/main/java/vn/tiki/appid/product/list/ProductListPresenter.java
@@ -1,21 +1,23 @@
 package vn.tiki.appid.product.list;
 
+import android.support.annotation.NonNull;
 import io.reactivex.Scheduler;
 import io.reactivex.Single;
-import io.reactivex.functions.BiConsumer;
 import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
+import ix.Ix;
+import ix.IxPredicate;
 import java.util.List;
 import vn.tiki.appid.common.base.MvpPresenter;
-import vn.tiki.appid.common.util.Lists;
 import vn.tiki.appid.data.entity.Page;
 import vn.tiki.appid.data.entity.Product;
 import vn.tiki.appid.data.exception.NetworkException;
 import vn.tiki.appid.data.model.ProductModel;
 
-import static vn.tiki.appid.common.util.Lists.listOf;
-import static vn.tiki.appid.common.util.Lists.spread;
+import static ix.Ix.concat;
+import static ix.Ix.from;
+import static ix.Ix.just;
 import static vn.tiki.appid.data.entity.LoadingItem.loadingItem;
 import static vn.tiki.appid.data.entity.RetryItem.retryItem;
 
@@ -28,7 +30,7 @@ public class ProductListPresenter extends MvpPresenter<ProductListView> {
   private final Scheduler workerScheduler;
   private final Scheduler uiScheduler;
   private Page nextPage = Page.firstPage();
-  private List<Product> products;
+  private Ix<Product> productIx;
 
   public ProductListPresenter(ProductModel model, Scheduler workerScheduler,
       Scheduler uiScheduler) {
@@ -45,27 +47,16 @@ public class ProductListPresenter extends MvpPresenter<ProductListView> {
     view.showLoading();
 
     autoDispose(model.products(nextPage)
-        .map(new Function<List<Product>, List<Object>>() {
-          @Override public List<Object> apply(List<Product> products) throws Exception {
-            cache(products);
-            return listOf(
-                spread(products),
-                new Object[] {
-                    loadingItem()
-                }
-            );
-          }
-        })
+        .map(
+            loadingItemAppending()
+        )
         .subscribeOn(workerScheduler)
         .observeOn(uiScheduler)
         .subscribe(new Consumer<List<Object>>() {
-          @Override public void accept(List<Object> products) throws Exception {
+          @Override public void accept(List<Object> items) throws Exception {
+            cache(items);
             increasePage();
-            final ProductListView productListView = getView();
-            if (productListView == null) {
-              return;
-            }
-            productListView.showProducts(products);
+            showItemsInView(items);
           }
         }, new Consumer<Throwable>() {
           @Override public void accept(Throwable throwable) throws Exception {
@@ -84,60 +75,37 @@ public class ProductListPresenter extends MvpPresenter<ProductListView> {
 
   void loadMoreProducts() {
     final Single<List<Product>> loadProducts = Single.zip(
-        Single.just(products),
+        Single.just(productIx.toList()),
         model.products(nextPage),
         new BiFunction<List<Product>, List<Product>, List<Product>>() {
           @Override public List<Product> apply(List<Product> products, List<Product> products2)
               throws Exception {
-            return Lists.merged(
-                products,
-                products2
-            );
+            return concat(
+                from(products),
+                from(products2)
+            ).toList();
           }
-        }).doOnEvent(new BiConsumer<List<Product>, Throwable>() {
-      @Override public void accept(List<Product> products, Throwable throwable) throws Exception {
-        if (products != null && !products.isEmpty()) {
-
-        }
-      }
-    });
+        });
 
     autoDispose(loadProducts
         .subscribeOn(workerScheduler)
         .observeOn(uiScheduler)
-        .map(new Function<List<Product>, List<Object>>() {
-          @Override public List<Object> apply(List<Product> products) throws Exception {
-            cache(products);
-            return listOf(
-                spread(products),
-                new Object[] {
-                    loadingItem()
-                }
-            );
-          }
-        })
+        .map(
+            loadingItemAppending()
+        )
         .subscribe(new Consumer<List<Object>>() {
-          @Override public void accept(List<Object> products) throws Exception {
+          @Override public void accept(List<Object> items) throws Exception {
+            cache(items);
             increasePage();
-            final ProductListView productListView = getView();
-            if (productListView == null) {
-              return;
-            }
-            productListView.showProducts(products);
+            showItemsInView(items);
           }
         }, new Consumer<Throwable>() {
           @Override public void accept(Throwable throwable) throws Exception {
-            final ProductListView productListView = getView();
-            if (productListView == null) {
-              return;
-            }
-            productListView.showProducts(
-                listOf(
-                    spread(products),
-                    new Object[] {
-                        retryItem()
-                    }
-                )
+            showItemsInView(
+                concat(
+                    productIx,
+                    just(retryItem())
+                ).toList()
             );
           }
         }));
@@ -147,7 +115,38 @@ public class ProductListPresenter extends MvpPresenter<ProductListView> {
     nextPage = nextPage.next();
   }
 
-  private void cache(List<Product> products) {
-    this.products = products;
+  private void cache(List<Object> items) {
+    productIx = from(items)
+        .filter(
+            isInstanceOfProduct()
+        )
+        .cast(Product.class);
+  }
+
+  private void showItemsInView(List<Object> items) {
+    final ProductListView productListView = getView();
+    if (productListView == null) {
+      return;
+    }
+    productListView.showProducts(items);
+  }
+
+  @NonNull private Function<List<Product>, List<Object>> loadingItemAppending() {
+    return new Function<List<Product>, List<Object>>() {
+      @Override public List<Object> apply(List<Product> products) throws Exception {
+        return concat(
+            from(products),
+            just(loadingItem())
+        ).toList();
+      }
+    };
+  }
+
+  @NonNull private IxPredicate<Object> isInstanceOfProduct() {
+    return new IxPredicate<Object>() {
+      @Override public boolean test(Object o) {
+        return o instanceof Product;
+      }
+    };
   }
 }
